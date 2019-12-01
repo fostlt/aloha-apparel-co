d1234238c4382667fd206b823af354c73f2d72b0
Lance <lanceadamp@gmail.com>
2019-11-30 17:07:56 -0800

    basic flickity finished

--- js/script.js
+++ js/script.js
@@ -1,48 +1,62 @@
-document.addEventListener("DOMContentLoaded", function() {  
-    // console.log("hello world")
+var elem = document.querySelector('.carousel-container');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
 
-    const elem = document.querySelector('.main-carousel');
-    const flkty = new Flickity( elem, {
-      // options
-      cellAlign: 'left',
-      wrapAround: true,
-      freeScroll: true
-    });
-
-
-
-    const  emailInput = document.getElementById("emailInput");
-    const  subBtn = document.getElementById("subBtn");
-    const  subForm = document.getElementById("emailInfo");
-
-    subForm.addEventListener("submit", function(event) {
-      event.preventDefault();
-      if (emailInput.value === "") {
-        alert("Please enter a valid email address!");
-      } else {
-        alert("Thanks for subscribing!");
-        emailInput.value = ""
-      }
-    }); 
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.carousel-container', {
+  // options
+});
 
 
-    const cartNumber = document.getElementById("cartNumber");
-    const cartBtn = document.getElementsByClassName("cartBtn");
+
+
+// document.addEventListener("DOMContentLoaded", function() {  
+//     // console.log("hello world")
+
+//     const elem = document.querySelector('.main-carousel');
+//     const flkty = new Flickity( elem, {
+//       // options
+//       cellAlign: 'left',
+//       wrapAround: true,
+//       freeScroll: true
+//     });
+
+
+
+//     const  emailInput = document.getElementById("emailInput");
+//     const  subBtn = document.getElementById("subBtn");
+//     const  subForm = document.getElementById("emailInfo");
+
+//     subForm.addEventListener("submit", function(event) {
+//       event.preventDefault();
+//       if (emailInput.value === "") {
+//         alert("Please enter a valid email address!");
+//       } else {
+//         alert("Thanks for subscribing!");
+//         emailInput.value = ""
+//       }
+//     }); 
+
+
+//     const cartNumber = document.getElementById("cartNumber");
+//     const cartBtn = document.getElementsByClassName("cartBtn");
     
-    let clicks = 0;
+//     let clicks = 0;
 
-    for(let i = 0; i < cartBtn.length; i++){
-      cartBtn[i].addEventListener("click", function addclicks() {
-        clicks++;
-        console.log(clicks);
-        cartNumber.innerHTML = clicks
-
-
-
-      });
-    }
+//     for(let i = 0; i < cartBtn.length; i++){
+//       cartBtn[i].addEventListener("click", function addclicks() {
+//         clicks++;
+//         console.log(clicks);
+//         cartNumber.innerHTML = clicks
 
 
+
+//       });
+//     }
 
 
 
@@ -69,4 +83,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-}); /// end of doc ready
+
+
+// }); /// end of doc ready
