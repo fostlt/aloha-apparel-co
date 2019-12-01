3ea663303bfaa6a26a5fe9968929b0f211a0cc1d
Lance <lanceadamp@gmail.com>
2019-12-01 00:13:36 -0800

    added js

--- js/script.js
+++ js/script.js
@@ -13,51 +13,35 @@ var flkty = new Flickity( '.carousel-container', {
 });
 
 
+    const  emailInput = document.getElementById("emailInput");
+    const  subForm = document.getElementById("emailInfo");
+
+    subForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+      if (emailInput.value === "") {
+        alert("Please enter a valid email address!");
+      } else {
+        alert("Thanks for subscribing!");
+        emailInput.value = ""
+      }
+    }); 
 
 
-// document.addEventListener("DOMContentLoaded", function() {  
-//     // console.log("hello world")
-
-//     const elem = document.querySelector('.main-carousel');
-//     const flkty = new Flickity( elem, {
-//       // options
-//       cellAlign: 'left',
-//       wrapAround: true,
-//       freeScroll: true
-//     });
-
-
-
-//     const  emailInput = document.getElementById("emailInput");
-//     const  subBtn = document.getElementById("subBtn");
-//     const  subForm = document.getElementById("emailInfo");
-
-//     subForm.addEventListener("submit", function(event) {
-//       event.preventDefault();
-//       if (emailInput.value === "") {
-//         alert("Please enter a valid email address!");
-//       } else {
-//         alert("Thanks for subscribing!");
-//         emailInput.value = ""
-//       }
-//     }); 
-
-
-//     const cartNumber = document.getElementById("cartNumber");
-//     const cartBtn = document.getElementsByClassName("cartBtn");
+    const cartNumber = document.getElementById("cartNumber");
+    const cartBtn = document.getElementsByClassName("cartBtn");
     
-//     let clicks = 0;
+    let clicks = 0;
 
-//     for(let i = 0; i < cartBtn.length; i++){
-//       cartBtn[i].addEventListener("click", function addclicks() {
-//         clicks++;
-//         console.log(clicks);
-//         cartNumber.innerHTML = clicks
+    for(let i = 0; i < cartBtn.length; i++){
+      cartBtn[i].addEventListener("click", function addclicks() {
+        clicks++;
+        console.log(clicks);
+        cartNumber.innerHTML = clicks
 
 
 
-//       });
-//     }
+      });
+    }
 
 
 
